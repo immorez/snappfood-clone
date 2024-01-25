00f390b014b62cb7ee1ab8a068139971ff3a9061
mqadamgahi <60408339+qadamgahiii@users.noreply.github.com>
2024-01-26 02:56:20 +0330

    fix: fix action payload type in error logger

--- src/redux/middlewares/errorLogger.ts
+++ src/redux/middlewares/errorLogger.ts
@@ -11,7 +11,7 @@ const undisplayedErrorCodes: string[] = [];
 const rtkQueryErrorLogger: Middleware = () => (next) => (action) => {
     // RTK Query uses `createAsyncThunk` from redux-toolkit under the hood, so we're able to utilize these matchers!
     if (isRejectedWithValue(action)) {
-        const { response } = action.payload;
+        const { response } = action.payload as any;
 
         const { config, data } = response;
 
